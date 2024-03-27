25be77bf5ae76cb06d53d3d8d1950ccddf5eee9c
Hevanafa <dhevansafa4@gmail.com>
2024-03-27 10:37:16 +0700

    First upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace YouTubeThumbnailDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbUrl = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txbLog = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbUrl
+            // 
+            this.txbUrl.Location = new System.Drawing.Point(13, 13);
+            this.txbUrl.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbUrl.Name = "txbUrl";
+            this.txbUrl.Size = new System.Drawing.Size(400, 27);
+            this.txbUrl.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(312, 48);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(100, 34);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txbLog
+            // 
+            this.txbLog.Location = new System.Drawing.Point(13, 89);
+            this.txbLog.Multiline = true;
+            this.txbLog.Name = "txbLog";
+            this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txbLog.Size = new System.Drawing.Size(400, 134);
+            this.txbLog.TabIndex = 3;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(13, 47);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 34);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 235);
+            this.Controls.Add(this.txbLog);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txbUrl);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "YouTube Thumbnail Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbUrl;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txbLog;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using AngleSharp.Html.Parser;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using System.Web;
+using System.Diagnostics;
+
+namespace YouTubeThumbnailDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            var thread = new Thread(() => {
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        //client.DownloadProgressChanged += Client_DownloadProgressChanged;
+                        client.DownloadFileCompleted += Client_DownloadFileCompleted;
+                        client.DownloadFileAsync(new Uri(txbUrl.Text), "temp.html");
+                    }
+                }
+                catch (Exception ex) {
+                    WriteLog("Error: " + ex.Message);
+                }
+            });
+
+            WriteLog("Starting download...");
+
+            thread.Start();
+        }
+
+        //private void Client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        // https://stackoverflow.com/questions/49981433/
+        void WriteLog(string text) {
+            txbLog.BeginInvoke(new Action(() =>
+                txbLog.Text += text + "\r\n"
+            ));
+        }
+
+        private async void Client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //MessageBox.Show("Saved as temp.html");
+
+            var uri = new Uri(txbUrl.Text);
+            var hash = HttpUtility.ParseQueryString(uri.Query).Get("v");
+            WriteLog(hash);
+
+            var sr = new StreamReader("temp.html");
+            var html = await sr.ReadToEndAsync();
+
+            var parser = new HtmlParser();
+            var document = parser.ParseDocument(html);
+
+            var node = document.QuerySelector("link[itemprop=\"thumbnailUrl\"]");
+            var imgHref = node.GetAttribute("href");
+            WriteLog("Found the link: " + imgHref);
+
+            var ext = Path.GetExtension(imgHref);
+            newFilename = hash + ext;
+            //WriteLog("Filename: " + newFilename);
+
+            // Actually download the image
+            if (!Directory.Exists("thumbs"))
+                Directory.CreateDirectory("thumbs");
+
+            using (var client = new WebClient()) {
+                client.DownloadFileCompleted += Client_DownloadFileCompleted1;
+                client.DownloadFileAsync(new Uri(imgHref), $"thumbs\\{newFilename}");
+            }
+
+            // Todo: handle the use case for different resolutions
+            // Done: handle the use case for livestreams
+        }
+
+        string newFilename;
+
+        private void Client_DownloadFileCompleted1(object sender, AsyncCompletedEventArgs e)
+        {
+            WriteLog("Saved as " + newFilename);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists("thumbs"))
+                Directory.CreateDirectory("thumbs");
+
+            Process.Start(".\\thumbs");
+        }
+    }
+}
